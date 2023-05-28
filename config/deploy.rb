dffc8178f943d39d765c480af168d0f04c8064a0
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-28 12:57:24 -0400

    Feat: seting deploy ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,20 @@ lock "~> 3.17.3"
 set :application, "myapp"
 set :repo_url, "git@github.com:davamo/howarts_rails.git"
 
+
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 set :deploy_to, "/home/deploy/#{fetch :application}"
+set :user, 'deploy'
+set :ssh_options, {
+    forward_agent: true,
+    auth_methods: ['publickey'],
+    keys: ['~/.ssh/aws_devChile.pem']
+}
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
