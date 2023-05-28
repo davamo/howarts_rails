8d043f96d116a2714944e5029ed35050347a6958
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-27 22:11:07 -0400

    Feat: add data seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+    Character.create(
+      name: Faker::Name.name,
+      location: Faker::Address.city,
+      house: Faker::Movies::HarryPotter.house
+    )
+  end
